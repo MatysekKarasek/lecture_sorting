7e2c0560386d7da390bcf28b39e350a0b084ce81
Matej Karasek <karasmatej@email.cz>
2024-04-17 08:57:53 +0200

    1

--- sorting.py
+++ sorting.py
@@ -1,20 +1,25 @@
 import os
-
-
+import csv
 def read_data(file_name):
-    """
-    Reads csv file and returns numeric data.
-
-    :param file_name: (str), name of CSV file
-    :return: (dict), dictionary with numeric data, keys - csv column names, values - numbers in each column
-    """
+    """ Reads csv file and returns numeric data.
+     :param file_name: (str), name of CSV file
+     :return: (dict), dictionary with numeric data, keys - csv column names, values - numbers in each column """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
-
+    with open(file_path, mode="r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        data = {} #"series_1": [], "series_2": [], "series_3": []
+        for row in reader:
+           for key in row.keys():
+                if key not in data:
+                    data[key] = [int(row[key])]
+                else:
+                    data[key].append(int(row[key]))
+    print(row)
+    return data
 
 def main():
-    pass
+    data = read_data("numbers.csv")
+    print(data)
 
-
-if __name__ == '__main__':
-    main()
+main()
